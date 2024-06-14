f5e4c7e083fdd66056295e4c1d8fd5f77c1ba5e0
Matus Chochlik <chochlik@gmail.com>
2024-06-14 22:34:47 +0200

    Changes in how packages are built + updated sub-module

---
+++ config/EAGinePackageECS.cmake
@@ -0,0 +1,12 @@
+#  Copyright Matus Chochlik.
+#  Distributed under the Boost Software License, Version 1.0.
+#  See accompanying file LICENSE_1_0.txt or copy at
+#  https://www.boost.org/LICENSE_1_0.txt
+#
+# Package specific options
+#  Debian
+#   Dependencies
+set(CPACK_DEBIAN_ECS-DEV_PACKAGE_DEPENDS "eagine-core-dev (>= @EAGINE_VERSION@)")
+#   Descriptions
+set(CPACK_DEBIAN_ECS-DEV_DESCRIPTION "C++ entity-component system.")
+
