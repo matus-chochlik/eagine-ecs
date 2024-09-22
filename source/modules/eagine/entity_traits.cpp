ccdecdad0d6d4117ba30f7ff4fd011b6b3c26830
Matus Chochlik <chochlik@gmail.com>
2024-09-22 07:53:55 +0200

    Added entity_traits for basic_identifier_value

--- source/modules/eagine/entity_traits.cpp
+++ source/modules/eagine/entity_traits.cpp
@@ -64,5 +64,23 @@ struct entity_traits<basic_identifier<M, B, C, T, V>> {
     }
 };
 //------------------------------------------------------------------------------
+export template <std::size_t M, std::size_t B, typename C, typename T, bool V>
+struct entity_traits<basic_identifier_value<M, B, C, T, V>> {
+    using identifier_type = basic_identifier<M, B, C, T, V>;
+
+    using value_type = basic_identifier_value<M, B, C, T, V>;
+
+    using parameter_type = const value_type;
+
+    [[nodiscard]] static constexpr auto first() noexcept -> value_type {
+        return {identifier_type{}.value()};
+    }
+
+    [[nodiscard]] static constexpr auto next(parameter_type i) noexcept
+      -> value_type {
+        return increment(i.identifier()).value();
+    }
+};
+//------------------------------------------------------------------------------
 } // namespace eagine::ecs
 
