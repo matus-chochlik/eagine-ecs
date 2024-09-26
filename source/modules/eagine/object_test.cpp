39a3b3116e1f4443e1d5f0e2c535efef60ed9160
Matus Chochlik <chochlik@gmail.com>
2024-09-26 06:27:44 +0200

    Added unit-tests for object

--- source/modules/eagine/object_test.cpp
+++ source/modules/eagine/object_test.cpp
@@ -48,6 +48,14 @@ struct greeting : eagine::ecs::component<"Greeting"> {
     std::string expression;
 };
 //------------------------------------------------------------------------------
+namespace eagine::ecs {
+
+template <bool Const>
+struct get_manipulator<::person, Const>
+  : std::type_identity<::person_manipulator<Const>> {};
+
+} // namespace eagine::ecs
+//------------------------------------------------------------------------------
 // ECS objects
 //------------------------------------------------------------------------------
 void object_create_destroy(eagitest::ctx_suite& s) {
@@ -149,8 +157,90 @@ void object_spawn_ensure_has(eagitest::ctx_suite& s) {
     }
 }
 //------------------------------------------------------------------------------
+void object_spawn_add_has(eagitest::ctx_suite& s) {
+    eagitest::case_ test{s, 4, "spawn & add & has"};
+    eagitest::track trck{test, 0, 2};
+
+    auto& m{enable_ecs(s.context()).value()};
+
+    m.register_component_storages<eagine::ecs::flat_map_cmp_storage, greeting>();
+
+    std::vector<eagine::ecs::object> objects;
+
+    const std::size_t count{1024Z};
+    objects.reserve(count);
+
+    while(objects.size() < count) {
+        objects.emplace_back(eagine::ecs::object::spawn(s.context()));
+
+        auto& obj{objects.back()};
+        obj.add(greeting("Hi"));
+
+        trck.checkpoint(1);
+    }
+
+    for(auto& obj : objects) {
+        test.check(obj.has<greeting>(), "has");
+        trck.checkpoint(2);
+    }
+}
+//------------------------------------------------------------------------------
 void object_spawn_ensure_remove(eagitest::ctx_suite& s) {
-    eagitest::case_ test{s, 4, "spawn & ensure & remove"};
+    eagitest::case_ test{s, 5, "spawn & ensure & remove"};
+    eagitest::track trck{test, 0, 6};
+
+    auto& m{enable_ecs(s.context()).value()};
+
+    m.register_component_storages<
+      eagine::ecs::flat_map_cmp_storage,
+      person,
+      greeting>();
+
+    std::vector<eagine::ecs::object> objects;
+
+    const std::size_t count{1024Z};
+    objects.reserve(count);
+
+    while(objects.size() < count) {
+        objects.emplace_back(eagine::ecs::object::spawn(s.context()));
+
+        auto& obj{objects.back()};
+        obj.ensure<greeting>()->expression = "Ahoy";
+        obj.ensure<person>().set("Marty", "McFly");
+
+        trck.checkpoint(1);
+    }
+
+    for(auto& obj : objects) {
+        test.check(obj.has_all<person, greeting>(), "has");
+        trck.checkpoint(2);
+    }
+
+    for(auto& obj : objects) {
+        obj.remove<greeting>();
+        trck.checkpoint(3);
+    }
+
+    for(auto& obj : objects) {
+        test.check(not obj.has<greeting>(), "has not greeting");
+        test.check(obj.has<person>(), "has person");
+        trck.checkpoint(4);
+    }
+
+    for(auto& obj : objects) {
+        obj.remove<person>();
+        trck.checkpoint(5);
+    }
+
+    for(auto& obj : objects) {
+        test.check(not obj.has<greeting>(), "has not greeting");
+        test.check(not obj.has<person>(), "has not person");
+        trck.checkpoint(6);
+    }
+}
+//------------------------------------------------------------------------------
+void object_spawn_ensure_forget(eagitest::ctx_suite& s) {
+    eagitest::case_ test{s, 6, "spawn & ensure & forgeet"};
     eagitest::track trck{test, 0, 4};
 
     auto& m{enable_ecs(s.context()).value()};
@@ -176,19 +266,23 @@ void object_spawn_ensure_remove(eagitest::ctx_suite& s) {
         trck.checkpoint(2);
     }
 
+    std::vector<eagine::identifier_value> entities;
+    entities.reserve(count);
+
     for(auto& obj : objects) {
-        obj.remove<greeting>();
+        entities.emplace_back(obj.entity());
+        obj.forget();
         trck.checkpoint(3);
     }
 
-    for(auto& obj : objects) {
-        test.check(not obj.has<greeting>(), "has not");
+    for(auto ent : entities) {
+        test.check(not m.knows(ent), "does not know");
         trck.checkpoint(4);
     }
 }
 //------------------------------------------------------------------------------
 void object_spawn_hide_show(eagitest::ctx_suite& s) {
-    eagitest::case_ test{s, 5, "spawn & hide & show"};
+    eagitest::case_ test{s, 7, "spawn & hide & show"};
     eagitest::track trck{test, 0, 2};
 
     auto& m{enable_ecs(s.context()).value()};
@@ -220,15 +314,130 @@ void object_spawn_hide_show(eagitest::ctx_suite& s) {
     }
 }
 //------------------------------------------------------------------------------
+void object_spawn_add_copy_from(eagitest::ctx_suite& s) {
+    eagitest::case_ test{s, 8, "spawn & add & copy_from"};
+    eagitest::track trck{test, 0, 2};
+
+    auto& m{enable_ecs(s.context()).value()};
+
+    m.register_component_storages<eagine::ecs::flat_map_cmp_storage, greeting>();
+
+    std::vector<eagine::ecs::object> objects;
+
+    const std::size_t count{1024Z};
+    objects.reserve(count);
+
+    objects.emplace_back(eagine::ecs::object::spawn(s.context()));
+    auto& first{objects.back()};
+    first.add(greeting("Ahoy"));
+
+    while(objects.size() < count) {
+        objects.emplace_back(eagine::ecs::object::spawn(s.context()));
+        auto& obj{objects.back()};
+        obj.copy_from<greeting>(objects.front());
+
+        trck.checkpoint(1);
+    }
+
+    for(auto& obj : objects) {
+        test.check(obj.has<greeting>(), "has");
+
+        trck.checkpoint(2);
+    }
+}
+//------------------------------------------------------------------------------
+void object_spawn_add_exchange_with(eagitest::ctx_suite& s) {
+    eagitest::case_ test{s, 9, "spawn & add & exchange_with"};
+    eagitest::track trck{test, 0, 3};
+
+    auto& m{enable_ecs(s.context()).value()};
+
+    m.register_component_storages<eagine::ecs::flat_map_cmp_storage, greeting>();
+
+    std::vector<eagine::ecs::object> src;
+    std::vector<eagine::ecs::object> dst;
+
+    const std::size_t count{1024Z};
+    src.reserve(count);
+    dst.reserve(count);
+
+    while(src.size() < count) {
+        src.emplace_back(eagine::ecs::object::spawn(s.context()));
+
+        auto& obj{src.back()};
+        obj.add(greeting("Hi"));
+
+        trck.checkpoint(1);
+    }
+
+    for(auto& obj : src) {
+        dst.emplace_back(eagine::ecs::object::spawn(s.context()));
+        test.check(not dst.back().has<greeting>(), "dst has not");
+        obj.exchange_with<greeting>(dst.back());
+        test.check(not obj.has<greeting>(), "src has not");
+
+        trck.checkpoint(2);
+    }
+
+    for(auto& obj : dst) {
+        test.check(obj.has<greeting>(), "has");
+
+        trck.checkpoint(3);
+    }
+}
+//------------------------------------------------------------------------------
+void object_spawn_add_write_read(eagitest::ctx_suite& s) {
+    eagitest::case_ test{s, 10, "spawn & add & write & read"};
+    eagitest::track trck{test, 0, 2};
+
+    auto& m{enable_ecs(s.context()).value()};
+
+    m.register_component_storages<
+      eagine::ecs::flat_map_cmp_storage,
+      person,
+      greeting>();
+
+    std::vector<eagine::ecs::object> objects;
+
+    const std::size_t count{1024Z};
+    objects.reserve(count);
+
+    while(objects.size() < count) {
+        objects.emplace_back(eagine::ecs::object::spawn(s.context()));
+        auto& obj{objects.back()};
+        obj.ensure<person>();
+        obj.write<person>(
+          [](eagine::ecs::object::entity_type, person_manipulator<false>& p) {
+              p.set("Marty", "McFly");
+          });
+
+        trck.checkpoint(1);
+    }
+
+    for(auto& obj : objects) {
+        obj.read<person>(
+          [&test, &trck](
+            eagine::ecs::object::entity_type, person_manipulator<true>& p) {
+              test.check(p.has_name("Marty", "McFly"), "has");
+              trck.checkpoint(2);
+          });
+    }
+}
+//------------------------------------------------------------------------------
 // main
 //------------------------------------------------------------------------------
 auto test_main(eagine::test_ctx& ctx) -> int {
-    eagitest::ctx_suite test{ctx, "object", 5};
+    eagitest::ctx_suite test{ctx, "object", 10};
     test.once(object_create_destroy);
     test.once(object_spawn_destroy);
     test.once(object_spawn_ensure_has);
+    test.once(object_spawn_add_has);
     test.once(object_spawn_ensure_remove);
+    test.once(object_spawn_ensure_forget);
     test.once(object_spawn_hide_show);
+    test.once(object_spawn_add_copy_from);
+    test.once(object_spawn_add_exchange_with);
+    test.once(object_spawn_add_write_read);
     return test.exit_code();
 }
 //------------------------------------------------------------------------------
